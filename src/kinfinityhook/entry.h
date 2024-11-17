c375b84d708b197dae03e6dcd226402999b42f68
Adam Berenik <adam.berenik@gmail.com>
2024-11-17 10:54:56 +0100

    NtQueryDirectoryFileEx in a standalone file. We are done with the refactoring now

--- src/kinfinityhook/entry.h
+++ src/kinfinityhook/entry.h
@@ -18,23 +18,6 @@
 #include "../Syshooker-Client/SyshookerCommon.h"
 
 ///
-/// Structures and typedefs.
-///
-
-typedef NTSTATUS(*NtQueryDirectoryFileEx_t)(
-	_In_ HANDLE FileHandle,
-	_In_opt_ HANDLE Event,
-	_In_opt_ PIO_APC_ROUTINE ApcRoutine,
-	_In_opt_ PVOID ApcContext,
-	_Out_ PIO_STATUS_BLOCK IoStatusBlock,
-	_Out_ PVOID FileInformation,
-	_In_ ULONG Length,
-	_In_ FILE_INFORMATION_CLASS FileInformationClass,
-	_In_ ULONG QueryFlags,
-	_In_opt_ PUNICODE_STRING FileName
-);
-
-///
 /// Forward declarations.
 ///
 
@@ -46,30 +29,3 @@ void DriverUnload(
 void __fastcall SyscallStub(
 	_In_ unsigned int SystemCallIndex, 
 	_Inout_ void** SystemCallFunction);
-
-NTSTATUS DetourNtQueryDirectoryFile(
-	_In_ HANDLE FileHandle,
-	_In_opt_ HANDLE Event,
-	_In_opt_ PIO_APC_ROUTINE ApcRoutine,
-	_In_opt_ PVOID ApcContext,
-	_Out_ PIO_STATUS_BLOCK IoStatusBlock,
-	_Out_ PVOID FileInformation,
-	_In_ ULONG Length,
-	_In_ FILE_INFORMATION_CLASS FileInformationClass,
-	_In_ BOOLEAN ReturnSingleEntry,
-	_In_opt_ PUNICODE_STRING FileName,
-	_In_ BOOLEAN RestartScan
-);
-
-NTSTATUS DetourNtQueryDirectoryFileEx(
-	_In_ HANDLE FileHandle,
-	_In_opt_ HANDLE Event,
-	_In_opt_ PIO_APC_ROUTINE ApcRoutine,
-	_In_opt_ PVOID ApcContext,
-	_Out_ PIO_STATUS_BLOCK IoStatusBlock,
-	_Out_ PVOID FileInformation,
-	_In_ ULONG Length,
-	_In_ FILE_INFORMATION_CLASS FileInformationClass,
-	_In_ ULONG QueryFlags,
-	_In_opt_ PUNICODE_STRING FileName
-);
