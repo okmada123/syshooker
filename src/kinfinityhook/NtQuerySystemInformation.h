aa9ba4a1796428b20e685dd3ec4c0c482f3cf698
Adam Berenik <adam.berenik@gmail.com>
2024-12-02 20:47:32 +0100

    NtQuerySystemInformation hook now prints process name

--- src/kinfinityhook/NtQuerySystemInformation.h
+++ src/kinfinityhook/NtQuerySystemInformation.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "Settings.h"
 
+// macro source: https://stackoverflow.com/questions/3437404/min-and-max-in-c
+#define MIN(a,b) (((a)<(b))?(a):(b))
+
 // source: https://learn.microsoft.com/en-us/windows/win32/api/winternl/nf-winternl-ntquerysysteminformation
 typedef struct _SYSTEM_PROCESS_INFORMATION {
     ULONG NextEntryOffset;
@@ -53,6 +56,13 @@ NTSTATUS DetourNtQuerySystemInformation(
         if (ProcessInformationPtr) {
             while (1) {
                 kprintf("[+] infinityhook: NtQuerySystemInformation: PID: %d\n", ProcessInformationPtr->UniqueProcessId);
+                WCHAR ProcessNameBuffer[MAX_PATH_SYSHOOKER] = { 0 };
+                wcsncpy(ProcessNameBuffer, ProcessInformationPtr->ImageName.Buffer, MIN(ProcessInformationPtr->ImageName.Length, MAX_PATH_SYSHOOKER));
+                /*for (size_t i = 0; i < ProcessInformationPtr->ImageName.Length / 2 && i < MAX_PATH_SYSHOOKER - 1; ++i) {
+                    ProcessNameBuffer[i] = (ProcessInformationPtr->FileName)[i];
+                }*/
+
+                kprintf("[+] infinityhook: NtQuerySystemInformation: Process Name: %ws\n", ProcessNameBuffer);
 
                 if (ProcessInformationPtr->NextEntryOffset == 0) break;
                 ProcessInformationPtr = (SYSTEM_PROCESS_INFORMATION*)((PUINT8)ProcessInformationPtr + ProcessInformationPtr->NextEntryOffset); // Move the pointer to the next structure (NextEntryOffset is in bytes, so calculate using pointer to 8bits)
