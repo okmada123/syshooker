da65d883f1e8fd686b3d53c719b1d01189d3971b
Adam Berenik <adam.berenik@gmail.com>
2025-02-28 14:12:58 +0100

    NtQueryKey class 4 - didnt find anything interesting there... not doing with NtQueryKey for now

--- src/kinfinityhook/NtQueryKey.h
+++ src/kinfinityhook/NtQueryKey.h
@@ -35,26 +35,40 @@ NTSTATUS DetourNtQueryKey(
 	_In_ ULONG Length,
 	_Out_ PULONG ResultLength)
 {
-	if (KeyInformationClass == 3) {
-		NTSTATUS status = OriginalNtQueryKey(KeyHandle, KeyInformationClass, KeyInformation, Length, ResultLength);
-		if (NT_SUCCESS(status)) {
-			kprintf("[+] infinityhook: NtQueryKey, class %d, after successful call, ResultLength: %d\n", KeyInformationClass, *ResultLength);
-			PKEY_NAME_INFORMATION KeyNameInformationPtr = (PKEY_NAME_INFORMATION)KeyInformation;
-			kprintf("[+] infinityhook: NtQueryKey: NameLength: %d\n", KeyNameInformationPtr->NameLength);
+	return OriginalNtQueryKey(KeyHandle, KeyInformationClass, KeyInformation, Length, ResultLength);
+	//if (KeyInformationClass == 3) {
+	//	NTSTATUS status = OriginalNtQueryKey(KeyHandle, KeyInformationClass, KeyInformation, Length, ResultLength);
+	//	if (NT_SUCCESS(status)) {
+	//		kprintf("[+] infinityhook: NtQueryKey, class %d, after successful call, ResultLength: %d\n", KeyInformationClass, *ResultLength);
+	//		PKEY_NAME_INFORMATION KeyNameInformationPtr = (PKEY_NAME_INFORMATION)KeyInformation;
+	//		kprintf("[+] infinityhook: NtQueryKey: NameLength: %d\n", KeyNameInformationPtr->NameLength);
 
-			wchar_t NameBuffer[MAX_PATH_SYSHOOKER] = { 0 };
-			for (size_t i = 0; i < KeyNameInformationPtr->NameLength && i < MAX_PATH_SYSHOOKER; ++i) {
-				NameBuffer[i] = KeyNameInformationPtr->Name[i];
-			}
-			kprintf("[+] infinityhook: NtQueryKey: Name: %ws\n", NameBuffer);
-		}
-		return status;
-	}
-	//else if (KeyInformationClass == 4) {;}
-	else {
-		//kprintf("[+] infinityhook: In Detoured NtQueryKey, class: %d\n", KeyInformationClass);
-		// call the original
-		return OriginalNtQueryKey(KeyHandle, KeyInformationClass, KeyInformation, Length, ResultLength);
-	}
+	//		wchar_t NameBuffer[MAX_PATH_SYSHOOKER] = { 0 };
+	//		for (size_t i = 0; i < KeyNameInformationPtr->NameLength && i < MAX_PATH_SYSHOOKER; ++i) {
+	//			NameBuffer[i] = KeyNameInformationPtr->Name[i];
+	//		}
+	//		kprintf("[+] infinityhook: NtQueryKey: Name: %ws\n", NameBuffer);
+	//	}
+	//	return status;
+	//}
+	//else if (KeyInformationClass == 4) {
+	//	NTSTATUS status = OriginalNtQueryKey(KeyHandle, KeyInformationClass, KeyInformation, Length, ResultLength);
+	//	if (NT_SUCCESS(status)) {
+	//		kprintf("[+] infinityhook: NtQueryKey, class %d, after successful call, ResultLength: %d\n", KeyInformationClass, *ResultLength);
+	//		PKEY_CACHED_INFORMATION KeyCachedInformationPtr = (PKEY_CACHED_INFORMATION)KeyInformation;
+	//		kprintf("[+] infinityhook: NtQueryKey: SubKeys: %ul, MaxNameLen: %ul, Values: %ul, NameLength: %ul\n", KeyCachedInformationPtr->SubKeys, KeyCachedInformationPtr->MaxNameLen, KeyCachedInformationPtr->Values, KeyCachedInformationPtr->NameLength);
 
+	//		/*wchar_t NameBuffer[MAX_PATH_SYSHOOKER] = { 0 };
+	//		for (size_t i = 0; i < KeyNameInformationPtr->NameLength && i < MAX_PATH_SYSHOOKER; ++i) {
+	//			NameBuffer[i] = KeyNameInformationPtr->Name[i];
+	//		}
+	//		kprintf("[+] infinityhook: NtQueryKey: Name: %ws\n", NameBuffer);*/
+	//	}
+	//	return status;
+	//}
+	//else {
+	//	//kprintf("[+] infinityhook: In Detoured NtQueryKey, class: %d\n", KeyInformationClass);
+	//	// call the original
+	//	//return OriginalNtQueryKey(KeyHandle, KeyInformationClass, KeyInformation, Length, ResultLength);
+	//}
 }
